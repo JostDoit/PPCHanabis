3fc1c37ae844655f465c3d3f1579f95df5b974cf
EnguerranC <enguerran.cheneau@gmail.com>
2024-01-23 16:10:39 +0100

    /

---
+++ game.py
@@ -0,0 +1,16 @@
+NOMBRE_JOUEURS = 3
+couleurs = ["rouge", "vert", "bleu", "jaune", "blanc"]
+
+class Carte :
+    def __init__(self, numero, couleur) :
+        self.numero = numero
+        self.couleur = couleur
+
+
+
+class Pioche :
+    global NOMBRE_JOUEURS
+    def __init__(self, NOMBRE_JOUEURS) :
+        self.pioche = []
+        for i in range (NOMBRE_JOUEURS) :
+            for i in range (10) :
